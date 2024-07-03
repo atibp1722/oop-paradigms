cc326bb399b0cd636802a3050afae5581e6ebbc2
Atib Pokharel <atibp1722@gmail.com>
2024-07-03 11:49:57 +0545

    simple program to demonstrate metaclass implementation of singleton by overriding call and init method

---
+++ singleton/metaclass-singleton.py
@@ -0,0 +1,34 @@
+#best implementation of singleton
+#defines rules and behaviours for creating class [class of class]
+#allow to customize class creation process to modify class attributes and methods before class actually created
+
+from typing import Any
+
+
+class MetaSingleton(type):
+    #dictionary that stores instance for each sub-class of the meta class 
+    _instances={}
+
+    #_call_ method allow class instance to be called as if they were functions
+    def __call__(self,*args,**kwargs):
+        #check if instance already created
+        if cls not in cls._instances:
+            #create instance using the _call_ method of parent class's _call_ method
+            instance=super().__call__(*args, **kwargs)
+            cls._instances[cls]=instance
+        return cls._instacnes[cls]
+    
+    def __init__(cls,name,bases,dct):
+        super().__init__(name,bases,dct)
+        cls._instances[cls]=super().__call__()
+    
+    #return instance of the singleton
+    def __call__(self,*args,**kwargs):
+        return cls._instances[cls]
+
+
+class Singleton(metaclass=MetaSingleton):
+
+    def __init__(self):
+        print('This is constrcutor of singleton class.')
+        
