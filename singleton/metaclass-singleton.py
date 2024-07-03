1eab8f42aecc00c86a9aa41f7f9122e709cc6287
Atib Pokharel <atibp1722@gmail.com>
2024-07-03 12:20:37 +0545

    made changes to metaclass singleton to override init and call methods examine timing of the method calls

--- singleton/metaclass-singleton.py
+++ singleton/metaclass-singleton.py
@@ -4,31 +4,44 @@
 
 from typing import Any
 
-
 class MetaSingleton(type):
     #dictionary that stores instance for each sub-class of the meta class 
     _instances={}
 
     #_call_ method allow class instance to be called as if they were functions
-    def __call__(self,*args,**kwargs):
+    def __call__(cls):
+        print('call running...')
         #check if instance already created
         if cls not in cls._instances:
             #create instance using the _call_ method of parent class's _call_ method
-            instance=super().__call__(*args, **kwargs)
+            instance=super().__call__()
             cls._instances[cls]=instance
-        return cls._instacnes[cls]
+        return cls._instances[cls]
     
     def __init__(cls,name,bases,dct):
         super().__init__(name,bases,dct)
         cls._instances[cls]=super().__call__()
     
     #return instance of the singleton
-    def __call__(self,*args,**kwargs):
+    def __call__(cls):
         return cls._instances[cls]
-
 
 class Singleton(metaclass=MetaSingleton):
 
     def __init__(self):
-        print('This is constrcutor of singleton class.')
-        
+        print('constructor of singleton...')
+        self.attrib='This is a singleton'
+
+sng=Singleton()
+sng1=Singleton()
+print(sng is sng1)
+
+print('-'*15)
+
+sngl=Singleton()
+sngl1=Singleton()
+
+print(sngl.attrib)
+print(sngl1.attrib)
+
+print(sngl is sngl1)
